bc1b165763aff1b9778cda845dd0d2023aed7009
Hardeep Bassi <hbassi21@berkeley.edu>
2021-10-20 11:27:02 -0700

    adding discussions

---
+++ Discussions/trapezoid.m
@@ -0,0 +1,9 @@
+function a = trapezoid(f,a,b,n)
+dx = (b-a)/n;
+xvals = linspace(a,b,n+1);
+fvals = arrayfun(f,xvals);
+wvals = dx*ones(size(xvals));
+wvals(1) = 0.5*wvals(1);
+wvals(end) = 0.5*wvals(end);
+intg = sum(fvals.*wvals);
+a = intg;
