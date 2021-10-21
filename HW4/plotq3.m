5ca7580a35ff0421f60dce91947eb3288fccb1ad
Hardeep Bassi <hbassi21@berkeley.edu>
2021-10-21 15:59:31 -0700

    modify figure 3 to show slopes

--- HW4/plotq3.m
+++ HW4/plotq3.m
@@ -16,12 +16,22 @@ for j = 1:10
     err2 = abs(app2-exactval);
     errs2 = [errs2 err2];
 end
+sl = @(x) x.^(-2);
+ys = sl(n);
+sl2 = @(x) x.^(-1);
+ys2= sl2(n);
 loglog(n, errs)
 hold on
 loglog(n,errs2)
 hold off
+hold on
+loglog(n,ys,'b--')
+hold off
+hold on 
+loglog(n,ys2, 'g--')
+hold off
 title('loglog of Errors vs n using Gauss-Legendre and Trapezoid Rule')
 xlabel('n')
 ylabel('error')
-legend('LG', 'Trapezoid')
+legend('LG', 'Trapezoid', 'slope of -2', 'slope of -1')
 saveas(gcf, '3.png')
