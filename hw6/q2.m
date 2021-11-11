181d7fbfbe3b44d8fd79b6cb53890e18db27c5ae
unknown <hbassi21@berkeley.edu>
2021-11-11 14:20:49 -0800

    modified  exact ode solution

--- hw6/q2.m
+++ hw6/q2.m
@@ -1,5 +1,5 @@
 F = @(t,u) (2*u-18*t)/(1+t);
-uexact = @(t) ((18+36*t+18*t^2)/(t+1)) - ((9 + 18*t - 9*t^2)/((t+1)^2)) -5*t^2 -10*t - 5;
+uexact = @(t) - 5*t^2 + 8*t + 4;
 [tvals100, uvals100] = Euler(F,0,2, 4, 100);
 [tvals200, uvals200] = Euler(F,0,2, 4, 200);
 [tvals500, uvals500] = Euler(F,0,2, 4, 500);
