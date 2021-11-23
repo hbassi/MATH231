818f4f08e314951bdf2706ddbf05426e55c859d4
Hardeep Bassi <hbassi21@berkeley.edu>
2021-11-23 14:21:51 -0800

    finished 3

--- hw7/test2ndode.m
+++ hw7/test2ndode.m
@@ -8,6 +8,10 @@ T = 10;
 [tvals50, yvals50] = solve_2nd_ode_by_RK4(a2,a1,f,t0,T,y0,dy0,50);
 [tvals100, yvals100] = solve_2nd_ode_by_RK4(a2,a1,f,t0,T,y0,dy0,100);
 [tvals200, yvals200] = solve_2nd_ode_by_RK4(a2,a1,f,t0,T,y0,dy0,200);
+syms y(t)
+[V] = odeToVectorField(diff(y, 2) == -diff(y)/10 - 10*sin(y));
+M = matlabFunction(V,'vars', {'t','Y'});
+[t,y] = ode45(M,[t0 T],[y0 dy0]);
 
 plot(tvals50, yvals50(:,1))
 hold on 
@@ -16,3 +20,11 @@ hold off
 hold on
 plot(tvals200, yvals200(:,1))
 hold off
+hold on 
+plot(t,y(:,1))
+hold off
+legend('n=50', 'n=100', 'n=200', 'True Solution')
+xlabel('t')
+ylabel('u')
+title('IVP Solution for Varying n')
+saveas(gcf,'3.png')
