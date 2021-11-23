866b8f433ed29e92ac80b5e9f847ae2207732207
Hardeep Bassi <hbassi21@berkeley.edu>
2021-11-23 11:41:04 -0800

    fixed 3

--- hw7/solve_2nd_ode_by_RK4.m
+++ hw7/solve_2nd_ode_by_RK4.m
@@ -1,6 +1,5 @@
 function [tvals, yvals] = solve_2nd_ode_by_RK4(a2,a1,f,t0,T,y0,dy0,n)
-
-F = @(t,y) [y(2) , ((-a1(t).*y(1)-f(t,y))./a2(t))];
+F = @(t,y) [y(2) , ((-a1(t).*y(2)-f(t,y(1)))./a2(t))];
 [tvals, yvals] = RK4(F, t0, T,[y0;dy0],n);
 
 
--- hw7/test2ndode.m
+++ hw7/test2ndode.m
@@ -9,10 +9,10 @@ T = 10;
 [tvals100, yvals100] = solve_2nd_ode_by_RK4(a2,a1,f,t0,T,y0,dy0,100);
 [tvals200, yvals200] = solve_2nd_ode_by_RK4(a2,a1,f,t0,T,y0,dy0,200);
 
-plot(tvals50, yvals50)
+plot(tvals50, yvals50(:,1))
 hold on 
-plot(tvals100, yvals100)
+plot(tvals100, yvals100(:,1))
 hold off
 hold on
-plot(tvals200, yvals200)
+plot(tvals200, yvals200(:,1))
 hold off
